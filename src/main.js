3fd8d18cc27a53e7f2f86772e9c81aa5f0d776a1
wcdaren <wcdaren@gmail.com>
2025-03-10 07:28:09 +0000

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./Router";
+import "./styles.css";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
